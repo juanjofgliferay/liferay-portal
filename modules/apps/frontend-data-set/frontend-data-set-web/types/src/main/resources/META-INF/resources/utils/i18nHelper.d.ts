a342b8f1a7a5d61c74beea2f21c49a3ae949e7d7
Juanjo Fernández <juanjo.fernandez@liferay.com>
2023-09-12 11:15:55 +0200

    LPS-195616 - Apply formatSource

--- modules/apps/frontend-data-set/frontend-data-set-web/types/src/main/resources/META-INF/resources/utils/i18nHelper.d.ts
+++ modules/apps/frontend-data-set/frontend-data-set-web/types/src/main/resources/META-INF/resources/utils/i18nHelper.d.ts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-export interface ILocalizedItemDetails {
+interface ILocalizedItemDetails {
 	rootPropertyName: string;
 	value: string;
 	valuePath: Array<string>;
@@ -12,3 +12,4 @@ export declare function getLocalizedValueFromItem(
 	item: any,
 	fieldName: string | Array<string>
 ): ILocalizedItemDetails | null;
+export {};
