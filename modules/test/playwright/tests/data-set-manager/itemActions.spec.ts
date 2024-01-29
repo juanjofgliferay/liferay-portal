1c632c07d6e33eac112a9fea148f4e3eedb2c068
Juanjo Fernández <juanjo.fernandez@liferay.com>
2024-01-29 16:25:14 +0100

    Drag & Drop by keyboard + format source

--- modules/test/playwright/tests/data-set-manager/itemActions.spec.ts
+++ modules/test/playwright/tests/data-set-manager/itemActions.spec.ts
@@ -6,204 +6,281 @@
 import {expect, mergeTests} from '@playwright/test';
 
 import {dataSetManagerPageTest} from '../../fixtures/DataSetManagerPages.fixture';
-// import {apiHelpersTest} from '../../fixtures/apiHelpersTest';
-import {loginTest} from '../../fixtures/loginTest';
 import {applicationsMenuPageTest} from '../../fixtures/applicationsMenuPageTest';
+import {loginTest} from '../../fixtures/loginTest';
 
 const DATASET_NAME = 'New Data Set Item Actions';
 
 export const test = mergeTests(
 	applicationsMenuPageTest,
 	dataSetManagerPageTest,
-    loginTest
+	loginTest
 );
 
 test.describe('DataSet Item Actions', () => {
-    test('Setup environment', async({
-        dataSetManagerPage,
-    }) => {
-        await test.step('Create Data Set', async() => {
-            await dataSetManagerPage.createTestDataSet({name: DATASET_NAME});
-        });
-        
-        await test.step('Create Data Set View', async () => {
-            await dataSetManagerPage.createTestDataSetView({dataSetName: DATASET_NAME});
-        });
-    });
+	test('Setup environment', async ({dataSetManagerPage}) => {
+		await test.step('Create Data Set', async () => {
+			await dataSetManagerPage.createTestDataSet({name: DATASET_NAME});
+		});
 
-    test.describe('DataSet Fields Tab', () => {      
-        test('Dataset Fields Tab is selected', async ({
-            dataSetManagerPage,
-            page,
-        }) => {
-            await dataSetManagerPage.gotoTestDataSet({name: DATASET_NAME});
-            await dataSetManagerPage.gotoTestDataSetView({dataSetName: DATASET_NAME});
-        
-            await test.step('Open Data Set Fields Tab', async() => {
-                await dataSetManagerPage.gotoDataSetFieldsTab();
-        
-                await expect(page.getByRole('heading', {name: 'Fields'})).toBeVisible();
-                await expect(page.getByText(/No fields added yet./)).toBeVisible();
-            });
-        });
-    
-        test('Can select Fields for the Dataset', async ({
-            dataSetManagerPage,
-            page,
-        }) => {
-            await dataSetManagerPage.gotoTestDataSet({name: DATASET_NAME});
-            await dataSetManagerPage.gotoTestDataSetView({dataSetName: DATASET_NAME});
+		await test.step('Create Data Set View', async () => {
+			await dataSetManagerPage.createTestDataSetView({
+				dataSetName: DATASET_NAME,
+			});
+		});
+	});
 
-            await test.step('Open Data Set Fields Tab', async() => {
-                await dataSetManagerPage.gotoDataSetFieldsTab();
-        
-                await expect(page.getByRole('heading', {name: 'Fields'})).toBeVisible();
-                await expect(page.getByText(/No fields added yet./)).toBeVisible();
-            });
+	test.describe('DataSet Fields Tab', () => {
+		test('Dataset Fields Tab is selected', async ({
+			dataSetManagerPage,
+			page,
+		}) => {
+			await dataSetManagerPage.gotoTestDataSet({name: DATASET_NAME});
+			await dataSetManagerPage.gotoTestDataSetView({
+				dataSetName: DATASET_NAME,
+			});
 
-            const fieldModal = await test.step('Open Data Set Fields Modal', async() => {
-                await dataSetManagerPage.addNewField();
-        
-                await expect(page.getByRole('dialog')).toBeVisible();
+			await test.step('Open Data Set Fields Tab', async () => {
+				await dataSetManagerPage.gotoDataSetFieldsTab();
 
-                await expect(page.getByRole('heading', {name: 'Add Fields'})).toBeVisible();
+				await expect(
+					page.getByRole('heading', {name: 'Fields'})
+				).toBeVisible();
+				await expect(
+					page.getByText('No fields added yet.')
+				).toBeVisible();
+			});
+		});
 
-                return page.getByRole('dialog');
-            });
+		test('Can select Fields for the Dataset', async ({
+			dataSetManagerPage,
+			page,
+		}) => {
+			await dataSetManagerPage.gotoTestDataSet({name: DATASET_NAME});
+			await dataSetManagerPage.gotoTestDataSetView({
+				dataSetName: DATASET_NAME,
+			});
 
-            await test.step('Select id and name fields', async() => {
-                await fieldModal.getByLabel(/id/).check();
-                await fieldModal.getByLabel(/name/).check();
-                await fieldModal.getByRole('button', {name: 'Save'}).click();
+			await test.step('Open Data Set Fields Tab', async () => {
+				await dataSetManagerPage.gotoDataSetFieldsTab();
 
-                await expect(page.locator('.orderable-table-row').nth(0)).toContainText('id');
-                await expect(page.locator('.orderable-table-row').nth(1)).toContainText('name');
-            });
-        });
-    });
+				await expect(
+					page.getByRole('heading', {name: 'Fields'})
+				).toBeVisible();
+				await expect(
+					page.getByText(/No fields added yet./)
+				).toBeVisible();
+			});
 
-    test.describe('DataSet Actions Tab', () => {
-        test('Dataset Actions Tab is selected', async ({
-            dataSetManagerPage,
-            page,
-        }) => {
-            await dataSetManagerPage.gotoTestDataSet({name: DATASET_NAME});
-            await dataSetManagerPage.gotoTestDataSetView({dataSetName: DATASET_NAME});
-        
-            await test.step('Open Data Set Actions Tab', async() => {
-                await dataSetManagerPage.gotoDataSetActionsTab();
-        
-                await expect(page.getByRole('heading', {name: 'Actions'})).toBeVisible();
-            });
-        });
+			const fieldModal =
+				await test.step('Open Data Set Fields Modal', async () => {
+					await dataSetManagerPage.addNewField();
 
-        test('Can define an Item Action of type Link for the Dataset', async ({
-            dataSetManagerPage,
-            page,
-        }) => {
-            await dataSetManagerPage.gotoTestDataSet({name: DATASET_NAME});
-            await dataSetManagerPage.gotoTestDataSetView({dataSetName: DATASET_NAME});
+					await expect(page.getByRole('dialog')).toBeVisible();
 
-            await test.step('Open Data Set Actions Tab', async() => {
-                await dataSetManagerPage.gotoDataSetActionsTab();
-        
-                await expect(page.getByRole('heading', {name: 'Actions'})).toBeVisible();
-            });
+					await expect(
+						page.getByRole('heading', {name: 'Add Fields'})
+					).toBeVisible();
 
-            await test.step('Open Data Set Item Action Form', async() => {
-                await dataSetManagerPage.addItemAction();
-        
-                await expect(page.getByRole('heading', {name: 'New Item Action'})).toBeVisible();
-            });
+					return page.getByRole('dialog');
+				});
 
-            await test.step('Fill Item Action Form', async() => {
-                await page.getByPlaceholder('Action Name').fill('Link Item Action');
+			await test.step('Select id and name fields', async () => {
+				await fieldModal.getByLabel(/id/).check();
+				await fieldModal.getByLabel(/name/).check();
+				await fieldModal.getByRole('button', {name: 'Save'}).click();
 
-                await page.getByLabel('TypeRequired', {exact: true}).selectOption({label: 'Link'});
+				await expect(
+					page.locator('.orderable-table-row').nth(0)
+				).toContainText('id');
+				await expect(
+					page.locator('.orderable-table-row').nth(1)
+				).toContainText('name');
+			});
+		});
+	});
 
-                await page.getByLabel('URLRequired', {exact: true}).fill('https://www.liferay.com');
+	test.describe('DataSet Actions Tab', () => {
+		test('Dataset Actions Tab is selected', async ({
+			dataSetManagerPage,
+			page,
+		}) => {
+			await dataSetManagerPage.gotoTestDataSet({name: DATASET_NAME});
+			await dataSetManagerPage.gotoTestDataSetView({
+				dataSetName: DATASET_NAME,
+			});
 
-                await page.getByRole('button', {name: 'Save'}).click();
-            });
+			await test.step('Open Data Set Actions Tab', async () => {
+				await dataSetManagerPage.gotoDataSetActionsTab();
 
-            await test.step('Action is defined',async () => {
-                await expect(page.locator('.orderable-table-row').nth(0)).toContainText('Link Item Action');
-            });
-        }); 
-    });
+				await expect(
+					page.getByRole('heading', {name: 'Actions'})
+				).toBeVisible();
+			});
+		});
 
-    test.describe('Dataset in the home page', () => {
-        test('Add DataSet fragment', async({applicationsMenuPage, page}) => {
-            await test.step('Go Home', async () => {
-                await applicationsMenuPage.goToHome();
-            });
+		test('Can define an Item Action of type Link for the Dataset', async ({
+			dataSetManagerPage,
+			page,
+		}) => {
+			await dataSetManagerPage.gotoTestDataSet({name: DATASET_NAME});
+			await dataSetManagerPage.gotoTestDataSetView({
+				dataSetName: DATASET_NAME,
+			});
 
-            await test.step('Click on "Edit" button', async () => {
-                const editPageButton = await page.getByRole('link', {name: 'Edit'});
-                await editPageButton.click();
-            });
+			await test.step('Open Data Set Actions Tab', async () => {
+				await dataSetManagerPage.gotoDataSetActionsTab();
 
-            await test.step('Search for "Data Set" fragment', async () => {
-                const fragmentSearchInput = await page.getByLabel('Search Fragments and Widgets');
-                fragmentSearchInput.fill('Data Set');
-            });
+				await expect(
+					page.getByRole('heading', {name: 'Actions'})
+				).toBeVisible();
+			});
 
-            await test.step('Drag "Data Set" fragment & Drop into the page editor', async () => {
-                const source = page.getByRole('menuitem', {name: 'Data Set'});
-                const target = page.locator('.page-editor__root');
+			await test.step('Open Data Set Item Action Form', async () => {
+				await dataSetManagerPage.addItemAction();
 
-                const pageEditor = await target.boundingBox();
-                const fragmentSidebarWidth = 350; // icon panel 42 + browser panel 280 + extra
+				await expect(
+					page.getByRole('heading', {name: 'New Item Action'})
+				).toBeVisible();
+			});
 
-                await source.dragTo(target, {
-                    targetPosition: {x: fragmentSidebarWidth, y: pageEditor.y}
-                });
-            });
+			await test.step('Fill Item Action Form', async () => {
+				await page
+					.getByPlaceholder('Action Name')
+					.fill('Link Item Action');
 
-            await test.step('Select empty Data Set fragment', async () => {
-                await page
-                    .getByText('Select a data set view. Beta')
-                    .first()
-                    .click();
-            });
+				await page
+					.getByLabel('TypeRequired', {exact: true})
+					.selectOption({label: 'Link'});
 
-            await test.step('Open Data Set View Selector', async () => {
-                await page.getByRole('button', {name: 'Select Data Set View'}).click();
-                await page.getByRole('menuitem', {name: 'Select Data Set View...'}).click();
-            });
+				await page
+					.getByLabel('URLRequired', {exact: true})
+					.fill('https://www.liferay.com');
 
-            await test.step('Select Data Set View', async () => {
-                await expect(page.getByRole('dialog')).toBeVisible();
-                await expect(page.getByRole('heading', {name: 'Select'})).toBeVisible();
-                await page.frameLocator('iframe[title="Select"]').locator('li').filter({ hasText: 'Data Set View Test' }).click();
-                await page.frameLocator('iframe[title="Select"]').getByRole('button', {name: 'Save'}).click();
-            });
+				await page.getByRole('button', {name: 'Save'}).click();
+			});
 
-            await test.step('Publish page with Data Set View', async () => {
-                await page.getByRole('button', {name: 'Publish'}).click();
+			await test.step('Action is defined', async () => {
+				await expect(
+					page.locator('.orderable-table-row').nth(0)
+				).toContainText('Link Item Action');
+			});
+		});
+	});
 
-                await applicationsMenuPage.goToHome();
+	test.describe('Dataset in the home page', () => {
+		test('Add DataSet fragment', async ({applicationsMenuPage, page}) => {
+			await test.step('Go Home', async () => {
+				await applicationsMenuPage.goToHome();
+			});
 
-                await expect(page.locator('.data-set-wrapper')).toBeInViewport();
-            });
+			await test.step('Click on "Edit" button', async () => {
+				const editPageButton = await page.getByRole('link', {
+					name: 'Edit',
+				});
+				await editPageButton.click();
+			});
 
-            await test.step('Item action are present in table row', async() => {
-                const tableRow = await page.locator('.dnd-td.item-actions').first();
-                await expect(tableRow.getByRole('link')).toBeVisible();
+			await test.step('Search for "Data Set" fragment', async () => {
+				const fragmentSearchInput = await page.getByLabel(
+					'Search Fragments and Widgets'
+				);
+				fragmentSearchInput.fill('Data Set');
+			});
 
-                await tableRow.getByRole('link').click();
+			// await test.step('Drag "Data Set" fragment & Drop into the page editor', async () => {
+			//     const source = page.getByRole('menuitem', {name: 'Data Set'});
+			//     const target = page.locator('.page-editor__root');
 
-                await page.waitForURL('https://www.liferay.com');
-                await expect(page.url()).toContain('https://www.liferay.com');
-            });
-            
-            // One button (unique action)
-            // Three dots (multiple actions)
+			//     const pageEditor = await target.boundingBox();
+			//     const fragmentSidebarWidth = 350; // icon panel 42 + browser panel 280 + extra
 
-        });
-    });
+			//     await source.dragTo(target, {
+			//         targetPosition: {x: fragmentSidebarWidth, y: pageEditor.y}
+			//     });
+			// });
 
-    test('Clean up environment', async ({dataSetManagerPage}) => {
-        await dataSetManagerPage.deleteDataSet({name: 'New Data Set Item Actions'});
-    });
+			await test.step('Drag "Data Set" fragment & Drop into the page editor w/ keyboard', async () => {
+				const source = await page.getByRole('menuitem', {
+					name: 'Data Set',
+				});
+				await source.focus();
+				await source.press('Enter');
+				await source.press('Enter');
+
+				// const target = page.locator('.page-editor__root');
+
+				// const pageEditor = await target.boundingBox();
+				// const fragmentSidebarWidth = 350; // icon panel 42 + browser panel 280 + extra
+
+				// await source.dragTo(target, {
+				//     targetPosition: {x: fragmentSidebarWidth, y: pageEditor.y}
+				// });
+
+			});
+
+			await test.step('Select empty Data Set fragment', async () => {
+				await page
+					.getByText('Select a data set view. Beta')
+					.first()
+					.click();
+			});
+
+			await test.step('Open Data Set View Selector', async () => {
+				await page
+					.getByRole('button', {name: 'Select Data Set View'})
+					.click();
+				await page
+					.getByRole('menuitem', {name: 'Select Data Set View...'})
+					.click();
+			});
+
+			await test.step('Select Data Set View', async () => {
+				await expect(page.getByRole('dialog')).toBeVisible();
+				await expect(
+					page.getByRole('heading', {name: 'Select'})
+				).toBeVisible();
+				await page
+					.frameLocator('iframe[title="Select"]')
+					.locator('li')
+					.filter({hasText: 'Data Set View Test'})
+					.first()
+					.click();
+				await page
+					.frameLocator('iframe[title="Select"]')
+					.getByRole('button', {name: 'Save'})
+					.click();
+			});
+
+			await test.step('Publish page with Data Set View', async () => {
+				await page.getByRole('button', {name: 'Publish'}).click();
+
+				await applicationsMenuPage.goToHome();
+
+				await expect(page.locator('.data-set-wrapper')).toBeVisible();
+			});
+
+			await test.step('Item action are present in table row', async () => {
+				const tableRow = await page
+					.locator('.dnd-td.item-actions')
+					.first();
+				await expect(tableRow.getByRole('link')).toBeVisible();
+
+				await tableRow.getByRole('link').click();
+
+				await page.waitForURL('https://www.liferay.com');
+				await expect(page.url()).toContain('https://www.liferay.com');
+			});
+
+			// One button (unique action)
+			// Three dots (multiple actions)
+
+		});
+	});
+
+	test('Clean up environment', async ({dataSetManagerPage}) => {
+		await dataSetManagerPage.deleteDataSet({
+			name: 'New Data Set Item Actions',
+		});
+	});
 });
