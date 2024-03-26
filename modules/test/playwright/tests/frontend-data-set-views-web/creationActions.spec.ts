057e6a616feedc8a103d14309783282215b96a3b
Juanjo Fernández <juanjo.fernandez@liferay.com>
2024-03-26 15:09:40 +0100

    LPD-20682 - Refactor. Update tests to use more robust locators.

--- modules/test/playwright/tests/frontend-data-set-views-web/creationActions.spec.ts
+++ modules/test/playwright/tests/frontend-data-set-views-web/creationActions.spec.ts
@@ -8,48 +8,89 @@ import {expect, mergeTests} from '@playwright/test';
 import {featureFlagsTest} from '../../fixtures/featureFlagsTest';
 import {loginTest} from '../../fixtures/loginTest';
 import {liferayConfig} from '../../liferay.config';
+import getRandomString from '../../utils/getRandomString';
 import {actionsPageTest} from './fixtures/actionsPageTest';
-import {dataSetsPageTest} from './fixtures/dataSetsPageTest';
+import {dataSetManagerApiHelpersTest} from './fixtures/dataSetManagerApiHelpersTest';
 import {fdsFragmentPageTest} from './fixtures/fdsFragmentPageTest';
-import {viewsPageTest} from './fixtures/viewsPageTest';
+import {DEFAULT_LABEL} from './utils/constants';
+
+const LINK_CREATION_ACTION_NAME = 'Link creation action';
+const MODAL_CREATION_ACTION_NAME = 'Modal creation action';
+const MODAL_CREATION_ACTION_TITLE = 'Modal creation title';
+const SIDE_PANEL_CREATION_ACTION_NAME = 'Side Panel creation action';
+const SIDE_PANEL_CREATION_ACTION_TITLE = 'Side Panel creation title';
 
 export const test = mergeTests(
 	actionsPageTest,
-	dataSetsPageTest,
+	dataSetManagerApiHelpersTest,
 	fdsFragmentPageTest,
 	featureFlagsTest({
 		'LPS-164563': true,
 		'LPS-178052': true,
 		'LPS-186871': true,
 	}),
-	loginTest(),
-	viewsPageTest
+	loginTest()
 );
 
-test.describe('Create Creation Actions on a Data Set and show them in a fragment', () => {
-	const LINK_CREATION_ACTION_NAME = 'Link creation action';
-	const MODAL_CREATION_ACTION_NAME = 'Modal creation action';
-	let site: Site;
-	let layout: Layout;
+let settingsDataSetERC: string;
+let settingsDataSetViewERC: string;
 
-	test('Create a Creation Action', async ({
+test.beforeEach(async ({dataSetManagerApiHelpers}) => {
+	settingsDataSetERC = getRandomString();
+	settingsDataSetViewERC = getRandomString();
+
+	await dataSetManagerApiHelpers.createDataSet({erc: settingsDataSetERC});
+	await dataSetManagerApiHelpers.createDataSetView({
+		erc: settingsDataSetViewERC,
+		r_fdsEntryFDSViewRelationship_c_fdsEntryERC: settingsDataSetERC,
+	});
+});
+
+test.afterEach(async ({dataSetManagerApiHelpers}) => {
+	await dataSetManagerApiHelpers.deleteDataSet({erc: settingsDataSetERC});
+});
+
+test.describe('Creation Actions in the Data Set Manager', () => {
+	test('There is a message if no Creation Action has been created', async ({
 		actionsPage,
-		dataSetsPage,
-		page,
-		viewsPage,
 	}) => {
-		await test.step('Create Data Set', async () => {
-			await dataSetsPage.goto();
-			await dataSetsPage.createDataSet();
+		await test.step('Navigate to the Actions tab', async () => {
+			await actionsPage.goto({
+				dataSetLabel: DEFAULT_LABEL.DATA_SET,
+				viewLabel: DEFAULT_LABEL.VIEW,
+			});
+
+			await expect(actionsPage.creationActionsTab).toBeInViewport();
 		});
 
-		await test.step('Create Data Set View', async () => {
-			await viewsPage.goto();
-			await viewsPage.createDataSetView();
+		await test.step('Navigate to the Creation Actions tab', async () => {
+			await actionsPage.creationActionsTab.click();
+			await actionsPage.newCreationActionButton.waitFor();
 		});
 
-		await test.step('Go to Actions tab', async () => {
-			await actionsPage.goto();
+		await test.step('Assert no Creation Actions are created', async () => {
+			await expect(actionsPage.noActionsWereCreatedMessage).toContainText(
+				'No actions were created.'
+			);
+		});
+	});
+
+	test('Can create a Creation Action of type Link', async ({
+		actionsPage,
+		page,
+	}) => {
+		await test.step('Navigate to the Actions tab', async () => {
+			await actionsPage.goto({
+				dataSetLabel: DEFAULT_LABEL.DATA_SET,
+				viewLabel: DEFAULT_LABEL.VIEW,
+			});
+
+			await expect(actionsPage.creationActionsTab).toBeInViewport();
+		});
+
+		await test.step('Navigate to the Creation Actions tab', async () => {
+			await actionsPage.creationActionsTab.click();
+			await actionsPage.newCreationActionButton.waitFor();
 		});
 
 		await test.step('Create a creation action', async () => {
@@ -62,169 +103,95 @@ test.describe('Create Creation Actions on a Data Set and show them in a fragment
 		});
 
 		await test.step('Check that the creation action is in the list', async () => {
+			await expect(actionsPage.creationActionsTab).toBeInViewport();
+
 			await expect(
-				page
-					.getByRole('cell', {
-						exact: true,
-						name: LINK_CREATION_ACTION_NAME,
-					})
-					.locator('span')
-					.first()
+				page.getByRole('cell', {
+					exact: true,
+					name: LINK_CREATION_ACTION_NAME,
+				})
 			).toBeVisible();
 		});
 	});
 
-	test('Show a single Creation Action in fragment', async ({
-		fdsFragmentPage,
-		page,
-	}) => {
-		await fdsFragmentPage.goto();
-
-		site = await fdsFragmentPage
-			.createSite('FDSFragment')
-			.then((response) => response);
-
-		layout = await fdsFragmentPage
-			.createPage({
-				siteId: site.id,
-				title: 'fdsfragmentpagetest',
-			})
-			.then((response) => response);
-
-		await page.reload();
-
-		await test.step('Edit page', async () => {
-			await fdsFragmentPage.editPage({layout, site});
-		});
-
-		await test.step('Search for "Data Set" fragment', async () => {
-			await fdsFragmentPage.searchFragmentOrWidget('Data Set');
-		});
-
-		await test.step('Drag "Data Set" fragment & Drop into the page editor w/ keyboard', async () => {
-			await fdsFragmentPage.dragAndDropFragment(
-				'Data Set Add Data Set Mark Data Set as Favorite'
-			);
-		});
-
-		await test.step('Select empty Data Set fragment', async () => {
-			await page
-				.getByText('Select a data set view. Beta')
-				.first()
-				.click();
-		});
-
-		await test.step('Open Data Set View Selector', async () => {
-			await page
-				.getByRole('button', {name: 'Select Data Set View'})
-				.click();
-		});
-
-		await test.step('Select Data Set View', async () => {
-			if (
-				await page
-					.getByRole('menuitem', {name: 'Select Data Set View...'})
-					.isVisible()
-			) {
-				await page
-					.getByRole('menuitem', {name: 'Select Data Set View...'})
-					.click();
-			}
-
-			await expect(page.getByRole('dialog')).toBeVisible();
-			await expect(
-				page.getByRole('heading', {name: 'Select'})
-			).toBeVisible();
-			await page
-				.frameLocator('iframe[title="Select"]')
-				.locator('li')
-				.filter({hasText: 'Data Set View Sample'})
-				.first()
-				.click();
-			await page
-				.frameLocator('iframe[title="Select"]')
-				.getByRole('button', {name: 'Save'})
-				.click();
-		});
-
-		await test.step('Publish page with Data Set View', async () => {
-			await fdsFragmentPage.publishPage();
-			await fdsFragmentPage.goToPage({layout, site});
-
-			await page.locator('.data-set-wrapper').waitFor();
-		});
-
-		await test.step('Creation action are present in fragment', async () => {
-			await expect(
-				page.getByLabel(LINK_CREATION_ACTION_NAME)
-			).toBeVisible();
-		});
-
-		await test.step('Creation action is actionable', async () => {
-			await page.getByLabel(LINK_CREATION_ACTION_NAME).click();
-
-			await expect(page.getByText('Welcome to Liferay')).toBeVisible();
-		});
-	});
-
-	test('Show Creation Actions menu in fragment', async ({
+	test('Can create a Creation Action of type Modal', async ({
 		actionsPage,
-		fdsFragmentPage,
 		page,
 	}) => {
-		await test.step('Go to Actions tab', async () => {
-			await actionsPage.goto();
+		await test.step('Navigate to the Actions tab', async () => {
+			await actionsPage.goto({
+				dataSetLabel: DEFAULT_LABEL.DATA_SET,
+				viewLabel: DEFAULT_LABEL.VIEW,
+			});
+
+			await expect(actionsPage.creationActionsTab).toBeInViewport();
 		});
 
-		await test.step('Create another creation action', async () => {
+		await test.step('Navigate to the Creation Actions tab', async () => {
+			await actionsPage.creationActionsTab.click();
+			await actionsPage.newCreationActionButton.waitFor();
+		});
+
+		await test.step('Create a creation action', async () => {
 			await actionsPage.createCreationAction({
 				icon: 'arrow-right-full',
 				name: MODAL_CREATION_ACTION_NAME,
+				title: MODAL_CREATION_ACTION_TITLE,
 				type: 'modal',
+				url: liferayConfig.environment.baseUrl,
+				variant: 'sm',
+			});
+		});
+
+		await test.step('Check that the creation action is in the list', async () => {
+			await expect(actionsPage.creationActionsTab).toBeInViewport();
+
+			await expect(
+				page.getByRole('cell', {
+					exact: true,
+					name: MODAL_CREATION_ACTION_NAME,
+				})
+			).toBeVisible();
+		});
+	});
+
+	test('Can create a Creation Action of type Side Panel', async ({
+		actionsPage,
+		page,
+	}) => {
+		await test.step('Navigate to the Actions tab', async () => {
+			await actionsPage.goto({
+				dataSetLabel: DEFAULT_LABEL.DATA_SET,
+				viewLabel: DEFAULT_LABEL.VIEW,
+			});
+
+			await expect(actionsPage.creationActionsTab).toBeInViewport();
+		});
+
+		await test.step('Navigate to the Creation Actions tab', async () => {
+			await actionsPage.creationActionsTab.click();
+			await actionsPage.newCreationActionButton.waitFor();
+		});
+
+		await test.step('Create a creation action', async () => {
+			await actionsPage.createCreationAction({
+				icon: 'arrow-right-full',
+				name: SIDE_PANEL_CREATION_ACTION_NAME,
+				title: SIDE_PANEL_CREATION_ACTION_TITLE,
+				type: 'sidePanel',
 				url: liferayConfig.environment.baseUrl,
 			});
 		});
 
 		await test.step('Check that the creation action is in the list', async () => {
+			await expect(actionsPage.creationActionsTab).toBeInViewport();
+
 			await expect(
-				page
-					.getByRole('cell', {
-						exact: true,
-						name: MODAL_CREATION_ACTION_NAME,
-					})
-					.locator('span')
-					.first()
+				page.getByRole('cell', {
+					exact: true,
+					name: SIDE_PANEL_CREATION_ACTION_NAME,
+				})
 			).toBeVisible();
-		});
-
-		await test.step('Go to new site home page', async () => {
-			await fdsFragmentPage.goToPage({layout, site});
-
-			await page.locator('.data-set-wrapper').waitFor();
-		});
-
-		await test.step('Creation action menu are present in fragment', async () => {
-			await page.getByLabel('New').click();
-			await expect(
-				page.getByRole('menuitem', {name: 'Link creation action'})
-			).toBeVisible();
-		});
-
-		await test.step('Creation action is actionable', async () => {
-			await page
-				.getByRole('menuitem', {name: 'Link creation action'})
-				.click();
-
-			await expect(page.getByText('Welcome to Liferay')).toBeVisible();
-		});
-	});
-
-	test('Sample data clean up', async ({dataSetsPage, fdsFragmentPage}) => {
-		await fdsFragmentPage.deleteSite(site.id);
-
-		await test.step('Delete Data Set', async () => {
-			await dataSetsPage.goto();
-			await dataSetsPage.deleteDataSet();
 		});
 	});
 });
