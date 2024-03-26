7b1d168236851cb43d2f427233a8bddc11bf276d
Juanjo Fernández <juanjo.fernandez@liferay.com>
2024-03-26 18:01:52 +0100

    LPD-20682 - Test. Add cases for no actions, one action, multiple actions

--- modules/test/playwright/tests/frontend-data-set-views-web/creationActions.spec.ts
+++ modules/test/playwright/tests/frontend-data-set-views-web/creationActions.spec.ts
@@ -5,7 +5,9 @@
 
 import {expect, mergeTests} from '@playwright/test';
 
+import {apiHelpersTest} from '../../fixtures/apiHelpersTest';
 import {featureFlagsTest} from '../../fixtures/featureFlagsTest';
+import {isolatedSiteTest} from '../../fixtures/isolatedSiteTest';
 import {loginTest} from '../../fixtures/loginTest';
 import {liferayConfig} from '../../liferay.config';
 import getRandomString from '../../utils/getRandomString';
@@ -194,4 +196,187 @@ test.describe('Creation Actions in the Data Set Manager', () => {
 			).toBeVisible();
 		});
 	});
+});
+
+export const fragmentTest = mergeTests(
+	apiHelpersTest,
+	dataSetManagerApiHelpersTest,
+	featureFlagsTest({
+		'LPS-164563': true,
+		'LPS-178052': true,
+	}),
+	fdsFragmentPageTest,
+	isolatedSiteTest
+);
+
+fragmentTest.describe('Creation Actions in the fragment', () => {
+	fragmentTest(
+		'Creation Action button does not appear if no creation action is defined',
+		async ({apiHelpers, fdsFragmentPage, site}) => {
+			const layout = await fragmentTest.step(
+				'Create a new page',
+				async () => {
+					const pageLayout =
+						await apiHelpers.headlessDelivery.createSitePage(
+							site.id,
+							getRandomString()
+						);
+
+					return pageLayout;
+				}
+			);
+
+			await fragmentTest.step(
+				'Configure Data Set in the page',
+				async () => {
+					await fdsFragmentPage.configureDataSetFragment({
+						layout,
+						site,
+					});
+				}
+			);
+
+			await fragmentTest.step(
+				'Check that the Creation Action button is not present',
+				async () => {
+					await expect(
+						fdsFragmentPage.creationMenuButton
+					).not.toBeVisible();
+				}
+			);
+		}
+	);
+
+	fragmentTest(
+		'Show a simple button if only one Creation Action is defined',
+		async ({
+			apiHelpers,
+			dataSetManagerApiHelpers,
+			fdsFragmentPage,
+			site,
+		}) => {
+			const actionLabel = 'Custom Creation Action';
+
+			fragmentTest.step('Create Creation Action', async () => {
+				dataSetManagerApiHelpers.createDataSetViewCreationAction({
+					label_i18n: {en_US: actionLabel},
+					r_fdsViewFDSCreationActionRelationship_c_fdsViewERC:
+						settingsDataSetViewERC,
+				});
+			});
+
+			const layout = await fragmentTest.step(
+				'Create a new page',
+				async () => {
+					const pageLayout =
+						await apiHelpers.headlessDelivery.createSitePage(
+							site.id,
+							getRandomString()
+						);
+
+					return pageLayout;
+				}
+			);
+
+			await fragmentTest.step(
+				'Configure Data Set in the page',
+				async () => {
+					await fdsFragmentPage.configureDataSetFragment({
+						layout,
+						site,
+					});
+				}
+			);
+
+			await fragmentTest.step(
+				'Check that the Creation Action button is present',
+				async () => {
+					await expect(
+						fdsFragmentPage.page.getByRole('button', {
+							name: actionLabel,
+						})
+					).toBeVisible();
+				}
+			);
+		}
+	);
+
+	fragmentTest(
+		'Show the Creation Actions menu if more than one Creation Action is defined',
+		async ({
+			apiHelpers,
+			dataSetManagerApiHelpers,
+			fdsFragmentPage,
+			site,
+		}) => {
+			const firstActionLabel = 'Custom Creation Action';
+			const secondActionLabel = 'Another Creation Action';
+
+			fragmentTest.step('Create Creation Actions', async () => {
+				dataSetManagerApiHelpers.createDataSetViewCreationAction({
+					label_i18n: {en_US: firstActionLabel},
+					r_fdsViewFDSCreationActionRelationship_c_fdsViewERC:
+						settingsDataSetViewERC,
+				});
+
+				dataSetManagerApiHelpers.createDataSetViewCreationAction({
+					label_i18n: {en_US: secondActionLabel},
+					r_fdsViewFDSCreationActionRelationship_c_fdsViewERC:
+						settingsDataSetViewERC,
+				});
+			});
+
+			const layout = await fragmentTest.step(
+				'Create a new page',
+				async () => {
+					const pageLayout =
+						await apiHelpers.headlessDelivery.createSitePage(
+							site.id,
+							getRandomString()
+						);
+
+					return pageLayout;
+				}
+			);
+
+			await fragmentTest.step(
+				'Configure Data Set in the page',
+				async () => {
+					await fdsFragmentPage.configureDataSetFragment({
+						layout,
+						site,
+					});
+				}
+			);
+
+			await fragmentTest.step(
+				'Check that the Creation Action menu is present',
+				async () => {
+					await fdsFragmentPage.creationMenuButton.isVisible();
+
+					const button = await fdsFragmentPage.creationMenuButton;
+
+					const dropdownId = await button.evaluate((node) =>
+						node.getAttribute('aria-controls')
+					);
+
+					await button.click();
+
+					await fdsFragmentPage.page
+						.locator(`#${dropdownId}`)
+						.filter({has: fdsFragmentPage.page.getByRole('menu')})
+						.waitFor();
+
+					await expect(
+						fdsFragmentPage.page
+							.locator(`#${dropdownId}`)
+							.getByRole('menuitem')
+					).toHaveCount(2);
+
+					// await expect(fdsFragmentPage.page.getByRole('button', {name: actionLabel})).toBeVisible();
+
+				}
+			);
+		}
+	);
 });
