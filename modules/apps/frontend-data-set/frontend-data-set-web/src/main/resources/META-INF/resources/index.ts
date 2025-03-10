3b26a5bb6ac3d6b22a6e5856d139640e666fab86
Daniel Sanz <daniel.sanz@liferay.com>
2025-03-10 18:35:57 +0100

    LPD-50120 Add an isVisible() optional JS function to the item action type

--- modules/apps/frontend-data-set/frontend-data-set-web/src/main/resources/META-INF/resources/index.ts
+++ modules/apps/frontend-data-set/frontend-data-set-web/src/main/resources/META-INF/resources/index.ts
@@ -98,6 +98,7 @@ export interface IItemsActions {
 	href?: string;
 	icon?: string;
 	id?: string | number;
+	isVisible?: (item: any) => boolean;
 	items?: IItemsActions[];
 	label?: string;
 	method?: string;
